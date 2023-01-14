b95723f1b24183ea9ee9b07b53865bd10969fc9b
lmbsumatra <122442812+lmbsumatra@users.noreply.github.com>
2023-01-14 18:46:29 +0800

    push

--- balance-inq.py
+++ balance-inq.py
@@ -1,6 +1,10 @@
 def balanceInq():
     print("Balance Inquiry\n")
 
-    print("\nAcount")
+    print("\nAccount Number: ")
+
+    print("Your balance: ")
+
+    print("After reviewing your account balance,\nenter [C] to continue.")
 
 balanceInq()
